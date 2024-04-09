1044ba83922aad1f9d284838c19999a3a59413f0
Gustavo Varella <gustavovarella8@gmail.com>
2024-04-09 15:28:10 -0300

    Lógica de programação em Python - Início

---
+++ Operadores.py
@@ -0,0 +1,15 @@
+A = 5
+B = 15
+C = 20
+
+print("A == B AND B > C: ", A == B and B > C)
+print("A < B AND B > C: ", A < B and B > C)
+print("not A == B: ", not A == B)
+
+idade = int(input("Digite sua idade: "))
+if idade >= 18:
+    print("Maior de idade")
+else:
+    print("Menor de idade")
+
+
---
+++ TipoVariavel.py
@@ -0,0 +1,9 @@
+codigo = int(input("Qual o código do funcionário: "))
+nome = (input("Digite o nome do funcioário: "))
+salario = float(input("Qual o salário do funcionário: "))
+ativo = True
+
+print("Código: %d " % codigo)
+print("Nome: %s " % nome)
+print("Salário: %.2f " % salario)
+print("Ativo: %r " % ativo)
