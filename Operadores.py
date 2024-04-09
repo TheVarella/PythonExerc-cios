7133213cf9f295d29f9457ce30d339a8ec22d43d
Gustavo Varella <gustavovarella8@gmail.com>
2024-04-09 15:32:40 -0300

    Lógica de programação em Python - Início

--- Operadores.py
+++ Operadores.py
@@ -3,7 +3,7 @@ B = 15
 C = 20
 
 print("A == B AND B > C: ", A == B and B > C)
-print("A < B AND B > C: ", A < B and B > C)
+print("A < B AND B < C: ", A < B and B < C)
 print("not A == B: ", not A == B)
 
 idade = int(input("Digite sua idade: "))
